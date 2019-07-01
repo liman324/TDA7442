af82e8552f01dfd6ef5dd41e2e5584f184f0edf2
Alexander Liman <liman324@yandex.ru>
2019-07-01 12:56:36 +0600

    Update TDA7442.ino

--- TDA7442.ino
+++ TDA7442.ino
@@ -11,14 +11,14 @@ void loop(){}
 
 void audio(){
  tda.setInput_att(0);  // int -63...0 = -31.5...0 dB step 0.5 dB
- tda.setSurr_sel(0, 0, 0);/* int 0...2, int 0...1, int 0...15:
+ tda.setSurr_sel(2, 1, 0);/* int 0...2, int 0...1, int 0...15:
                                                            SIMULATED STEREO | MUSIC | OFF
                                                            VAR | FIX
                                                            EFFECT CONTROL -6...-21 dB  */
- tda.setRez(0);  // int 0...3 = 12, 14, 18, 37 kOm
- tda.setBass(0);   // int -7...7 = -14...+14 dB step 2 dB
- tda.setTreble(0); // int -7...7 = -14...+14 dB step 2 dB
+ tda.setRez(3);  // int 0...3 = 12, 14, 18, 37 kOm
+ tda.setBass(4);   // int -7...7 = -14...+14 dB step 2 dB
+ tda.setTreble(5); // int -7...7 = -14...+14 dB step 2 dB
  tda.setVol_L(-20); // int -79...0 = -79...0 dB
  tda.setVol_R(-20); // int -79...0 = -79...0 dB
- tda.setInput(0);
+ tda.setInput(1); // int 1...4
  }
